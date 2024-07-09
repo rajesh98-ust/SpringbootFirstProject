5275aba550f31d125d709b80bcd9393708501665
rajesh98-ust <rajesh98.ust@gamil.com>
2024-07-09 23:45:20 +0530

    third commit

--- src/main/java/com/server/SpringbootFirstProjectApplication.java
+++ src/main/java/com/server/SpringbootFirstProjectApplication.java
@@ -4,11 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
-public class SpringbootFirstProjectApplication {
+public class SpringbootFirstProjectApplication extends SpringBootServletInitializer {
 	
 	static Logger log=LoggerFactory.getLogger(SpringbootFirstProjectApplication.class);
 	
