708aa9c6d53fd926b646acda6d1d3dbbded77820
rajesh98-ust <rajesh98.ust@gamil.com>
2024-07-09 23:16:47 +0530

    first commit

---
+++ src/main/java/com/server/SpringbootFirstProjectApplication.java
@@ -0,0 +1,25 @@
+package com.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringbootFirstProjectApplication {
+	
+	static Logger log=LoggerFactory.getLogger(SpringbootFirstProjectApplication.class);
+	
+	@PostConstruct
+	public void message() {
+		log.info("Rajesh Kumar");
+	}
+
+	public static void main(String[] args) {
+		log.info("Rajesh Kumar First jenkins ");
+		SpringApplication.run(SpringbootFirstProjectApplication.class, args);
+	}
+
+}
